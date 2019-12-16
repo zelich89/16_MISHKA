46f39c13ebd581e8f56f67bae02f1941186e86dd
unknown <C:\Users\Zelich\AppData\Roaming\The Bat!>
2019-12-16 19:56:16 +0200

    19:56 16/12

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ function watch() {
         }
     });
     gulp.watch('./sass/**/*.scss', style);
-    gulp.watch('./nonon.html').on('change', browserSync.reload);
+    gulp.watch('./*.html').on('change', browserSync.reload);
     gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 
